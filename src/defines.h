d36480e0db98118d448c75d5e2ba18c6303eb4ed
2dice <rjhxx419@gmail.com>
2014-05-13 21:52:35 +0900

    書く型のサイズをコメント・型のdefineを追加

--- src/defines.h
+++ src/defines.h
@@ -4,8 +4,16 @@
 #define NULL ((void *)0)
 #define SERIAL_DEFAULT_DEVICE 1
 
+/* char      :8bit */
+/* int,short :16bit */
+/* long      :32bit */
+
 typedef unsigned char  uint8;
 typedef unsigned short uint16;
 typedef unsigned long  uint32;
+
+typedef char int8;
+typedef short int16;
+typedef long  int32;
 
 #endif
