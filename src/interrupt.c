33cdb93fbcfb83827feb5c31e711a21bb158468e
2dice <rjhxx419@gmail.com>
2014-05-25 14:58:12 +0900

    リファクタリング(intr関数をmain.cからinterrupt.cに移動)

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,6 +1,50 @@
 #include "defines.h"
 #include "intr.h"
 #include "interrupt.h"
+#include "lib.h"
+#include "command.h"
+#include "peripheralInterface.h"
+
+static void intr(softvec_type_t type, uint32 sp)
+    {
+      int16 c;
+      static int8 command[32];
+      static int16 len;
+      
+      /* TODO:シリアル受信割込みハンドラへ登録 */
+      /* if((int)sizeof(command) < get_string(command)) */
+      /*   put_string("command too long\n"); */
+      c = get_char();
+      if(c != '\n')
+        {
+          command[len++] = c;
+        }
+      else{
+        if(!string_compare_at_arbitrary_length(command, "load", 4))
+        {
+          command_load();
+        }
+        else if(!string_compare_at_arbitrary_length(command, "dump", 4))
+        {
+          command_dump();
+        }
+        else if(!string_compare_at_arbitrary_length(command, "run", 3))
+        {
+          command_run();
+        }
+        else if(!string_compare_at_arbitrary_length(command, "echo", 4))
+        {
+          put_string(command + 4);
+          put_string("\n");
+        }
+        else
+        {
+          put_string("unknown.\n");
+        }
+        put_string("kzload> ");
+        len = 0;
+      }
+    }
 
 /* ソフトウェア割り込みベクタの初期化 */
 int16 softvec_init(void)
--- src/main.c
+++ src/main.c
@@ -6,47 +6,6 @@
 #include "intr.h"
 #include "serial.h"
 
-static void intr(softvec_type_t type, uint32 sp)
-    {
-      int16 c;
-      static int8 command[32];
-      static int16 len;
-      
-      /* TODO:シリアル受信割込みハンドラへ登録 */
-      /* if((int)sizeof(command) < get_string(command)) */
-      /*   put_string("command too long\n"); */
-      c = get_char();
-      if(c != '\n')
-        {
-          command[len++] = c;
-        }
-      else{
-        if(!string_compare_at_arbitrary_length(command, "load", 4))
-        {
-          command_load();
-        }
-        else if(!string_compare_at_arbitrary_length(command, "dump", 4))
-        {
-          command_dump();
-        }
-        else if(!string_compare_at_arbitrary_length(command, "run", 3))
-        {
-          command_run();
-        }
-        else if(!string_compare_at_arbitrary_length(command, "echo", 4))
-        {
-          put_string(command + 4);
-          put_string("\n");
-        }
-        else
-        {
-          put_string("unknown.\n");
-        }
-        put_string("kzload> ");
-        len = 0;
-      }
-    }
-
 static int init(void)
 {
   extern int data_start_load, data_start, edata, bss_start, ebss;
