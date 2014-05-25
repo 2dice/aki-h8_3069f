52d97e49c98c7f35152d246683dd499c8103ead3
2dice <rjhxx419@gmail.com>
2014-05-25 15:35:21 +0900

    リファクタリング(シリアル受信割り込みベクタの名前を変更)

--- src/vector.c
+++ src/vector.c
@@ -3,7 +3,7 @@
 extern void start(void);
 extern void intr_softerr(void);
 extern void intr_syscall(void);
-extern void intr_serintr(void);
+extern void RXI1(void);
 
 void (*vectors[])(void) = {
   start, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
@@ -13,6 +13,6 @@ void (*vectors[])(void) = {
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
   NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-  NULL, intr_serintr, NULL, NULL,
+  NULL, RXI1, NULL, NULL,
   NULL, NULL, NULL, NULL
 };
