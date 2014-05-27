a6efd6ddac680bf9730fbcdb0d3f6f146103177b
2dice <rjhxx419@gmail.com>
2014-05-27 23:08:03 +0900

    リファクタリング(戻り値が不要な関数の戻り値をvoidに変更)

--- src/serial.c
+++ src/serial.c
@@ -1,52 +1,44 @@
 #include "defines.h"
 #include "serial.h"
 
-int16 disable_SCI0_TxRx(void)
+void disable_SCI0_TxRx(void)
     {
       SCI0_SCR = SCI0_SCR & ~0b00110000;
-      return 0;
     }
 
-int16 disable_SCI0_serial_interrupt(void)
+void disable_SCI0_serial_interrupt(void)
     {
       SCI0_SCR = SCI0_SCR & ~0b11001100;
-      return 0;
     }
 
-int16 set_SCI0_clock_source_and_SCK_port_status(void)
+void set_SCI0_clock_source_and_SCK_port_status(void)
     {
       SCI0_SCR = SCI0_SCR & ~0b00000011;
-      return 0;
     }
 
-int16 set_SCI0_serial_modes(void)
+void set_SCI0_serial_modes(void)
     {
       SCI0_SMR = SCI0_SMR & ~0b11111111;
-      return 0;
     }
 
-int16 set_SCI0_bitrate(void)
+void set_SCI0_bitrate(void)
     {
       SCI0_BRR = 64;
-      return 0;
     }
 
-int16 enable_SCI0_serial_interrept(void)
+void enable_SCI0_serial_interrept(void)
     {
       SCI0_SCR = SCI0_SCR | 0b01000000;
-      return 0;
     }
 
-int16 enable_SCI0_TxRx(void)
+void enable_SCI0_TxRx(void)
     {
       SCI0_SCR = SCI0_SCR | 0b00110000;
-      return 0;
     }
 
-static int16 set_SCI0_sending(void)
+static void set_SCI0_sending(void)
     {
       SCI0_SSR = SCI0_SSR & ~0b10000000;
-      return 0;
     }
 
 void serial_send_byte(uint8 c)
@@ -57,10 +49,9 @@ void serial_send_byte(uint8 c)
   set_SCI0_sending();
 }
 
-static int16 set_SCI0_receiving(void)
+static void set_SCI0_receiving(void)
     {
       SCI0_SSR = SCI0_SSR & ~0b01000000;
-      return 0;
     }
 
 uint8 serial_recv_byte(void)
